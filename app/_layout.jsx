50433fb3f1f17bda16a2fbbd20f72d7979dbe6e9
uthamprabhu <uthamprabhu1@gmail.com>
2024-11-13 17:01:27 +0530

    deleted some ts files

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,11 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Slot, Stack } from 'expo-router'
 
 const RootLayout = () => {
   return (
-    <View style={styles.container}>
-      <Text>RootLayout</Text>
-    </View>
+    <Stack>
+      <Stack.Screen name='index' options={{ headerShown: false}} />
+    </Stack>
   )
 }
 
