78fc55be5b47560c6834c95705c951d891cd4e8e
uthamprabhu <uthamprabhu1@gmail.com>
2024-11-20 20:42:17 +0530

    login and signup appwrite integration done

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView, Image } from 'react-native'
+import { View, Text, ScrollView, Image, Alert } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
@@ -6,7 +6,8 @@ import { images } from '../../constants'
 import FormField from '../../components/FormField'
 
 import CustomButton from '../../components/CustomButton'
-import { Link } from 'expo-router'
+import { Link, router } from 'expo-router'
+import { signIn } from '../../lib/appwrite'
 
 const SignIn = () => {
 
@@ -17,8 +18,28 @@ const SignIn = () => {
 
   const [isSubmitting, setIsSubmitting] = useState(false)
 
-  const submit = () => {
+  const submit = async () => {
+    if (!form.email || !form.password) {
+      Alert.alert('Error', 'Please fill in all the fields')
+    }
 
+    setIsSubmitting(true)
+
+    try {
+      await
+        signIn(
+          form.email,
+          form.password
+        )
+
+      // set it to global set using Context
+      router.replace('/home')
+    } catch (error) {
+      Alert.alert('Error', error.messsage)
+      console.log(error)
+    } finally {
+      setIsSubmitting(false)
+    }
   }
 
   return (
--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,5 +1,5 @@
 import { createUser } from '../../lib/appwrite'
-import { View, Text, ScrollView, Image } from 'react-native'
+import { View, Text, ScrollView, Image, Alert } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
@@ -7,7 +7,7 @@ import { images } from '../../constants'
 import FormField from '../../components/FormField'
 
 import CustomButton from '../../components/CustomButton'
-import { Link } from 'expo-router'
+import { Link, router } from 'expo-router'
 
 
 const SignUp = () => {
@@ -20,8 +20,29 @@ const SignUp = () => {
 
   const [isSubmitting, setIsSubmitting] = useState(false)
 
-  const submit = () => {
-    createUser()
+  const submit = async () => {
+    if (!form.username || !form.email || !form.password) {
+      Alert.alert('Error', 'Please fill in all the fields')
+    }
+
+    setIsSubmitting(true)
+
+    try {
+      const result = await
+        createUser(
+          form.email,
+          form.password,
+          form.username
+        )
+
+      // set it to global set using Context
+      router.replace('/home')
+    } catch (error) {
+      Alert.alert('Error', error.messsage)
+      console.log(error)
+    } finally {
+      setIsSubmitting(false)
+    }
   }
 
   return (
