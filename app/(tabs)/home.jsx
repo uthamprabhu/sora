be9c6202867d90b9fc83aac23d5f69a69b141d38
uthamprabhu <uthamprabhu1@gmail.com>
2024-11-28 16:40:02 +0530

    profile page fully done

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -9,8 +9,11 @@ import { Alert } from 'react-native'
 import { getAllPosts, getLatestPosts } from '../../lib/appwrite'
 import useAppwrite from '../../lib/useAppwrite'
 import VideoCard from '../../components/VideoCard'
+import { useGlobalContext } from '../../context/GlobalProvider'
 
 const Home = () => {
+  const { user } = useGlobalContext()
+
   const { data: posts, refetch } = useAppwrite(getAllPosts)
 
   const { data: latestPosts } = useAppwrite(getLatestPosts)
@@ -44,7 +47,7 @@ const Home = () => {
                   Welcome Back,
                 </Text>
                 <Text className="text-2xl font-psemibold text-white">
-                  Utham
+                  {user?.username}
                 </Text>
               </View>
               <View className="mt-1.5">
